6e6a3d6a2de2fe3b492adcefa2976a0e12aab6a8
Daniel Sørensen <Daniel@2changeerp.dk>
2020-02-24 10:13:47 +0100

    Basic 1,2 done

---
+++ BasicThreading/BasicThreading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThreading
+{
+
+
+    class Threprog
+    {
+
+        public void WorkThreadFunction()
+        {
+            //for loop that writing simple thread;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name} time to work");
+                
+            }
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Creating new object of the class Threprog
+            Threprog pg = new Threprog();
+            //Creating new threads with the method WorkThreadFunction
+            Thread th = new Thread(new ThreadStart(pg.WorkThreadFunction));
+            Thread th2 = new Thread(new ThreadStart(pg.WorkThreadFunction));
+            //Setting the threads name to Thread 1 & 2
+            th.Name = "Thread 1";
+            th2.Name = "Thread 2";
+            //Starting the threads
+            th.Start();
+            th2.Start();
+            //writing what thread we started
+            Console.WriteLine($"Starting {th.Name}");
+            Console.WriteLine($"Starting {th2.Name}");
+            //Waiting for th to finish before ending/Continue main thread
+            th.Join();
+            //Writing done to see if thread 1 finish before main trhead
+            Console.WriteLine("main thread done");
+            //Read key to close application
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ BasicThreadingOne/BasicThreadingOne/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThreadingOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main thread";
+            //Letting user know we started main thread, for debugging sake
+            Console.WriteLine($"{Thread.CurrentThread.Name} is started");
+            //Creating object og class thpro
+            ThPro pro = new ThPro();
+            ThPro pro2 = new ThPro();
+            //Creating object of thread with the new thread start that contains a methood called hashtageasy
+            //Instead of creating 2 methods, i could give HashTagEasy an object and pass in variables.
+            Thread th = new Thread(new ThreadStart(pro.HashTagEasy));
+            Thread th2 = new Thread(new ThreadStart(pro2.Easy));
+            //Naming the trhead
+            th.Name = "thread 1";
+            th2.Name = "thread 2";
+            //Starting the thread
+            th.Start();
+            th2.Start();
+            //Waiting for the thread to end
+            th.Join();
+            th2.Join();
+            //Testing to see if thread 1 finish before main thread
+            Console.WriteLine($"{Thread.CurrentThread.Name} is done");
+
+        }
+    }
+
+
+    class ThPro
+    {
+        public void HashTagEasy()
+        {
+
+            //Making a for loop writing 5x times
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($" C#-Threading is easy {Thread.CurrentThread.Name} is running");
+                //Waiting 1 min
+                Thread.Sleep(1000);
+
+                //Is it smart to use sleep?
+
+                //Well it depends, if you depend on thread 1 to finish before thread 2,
+                //maybe is not that great, because while 1 is sleeping 2 will do
+                //his work first
+                //You can use the priority to get 1 thread to finish faster than the other
+            }
+
+        }
+
+        public void Easy()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Even with more threads ... {Thread.CurrentThread.Name} is running");
+                Thread.Sleep(1000);
+            }
+        }
+
+    }
+}
